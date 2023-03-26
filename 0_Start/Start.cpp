949b25b0108b506e95c7661aa945c31dbdb9c817
sputnikel0221 <111990655+sputnikel0221@users.noreply.github.com>
2023-03-27 08:01:16 +0900

    Initial commit

---
+++ 0_Start/Start.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+static int s = 150;
+
+int main(){
+    int a = 19;
+    int b = 21;
+    int c = a+b;
+
+    int N[] = {1,2,3,4,5};
+
+    printf("GGGGGGG");
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CodingTest
+ CPP CodingTEST
